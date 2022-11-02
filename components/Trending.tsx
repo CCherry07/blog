a54437c6bfe2350f703e4f677d470eaa0e8c6b7a
CCherry7 <2405693142@qq.com>
2022-11-02 23:42:57 +0800

    feat:  type change

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Image from "next/image"
 import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline'
 
-function Trending({ result }) {
+function Trending({ result }:any) {
   return (
     <div className='hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 font-light
             cursor-pointer transition ease-out flex justify-between items-center'>
       <div className='space-y-0.5'>
         <p className='text-[#6e767d] text-xs font-medium'>{result.heading}</p>
         <h6 className='font-bold max-w-[250px] text-sm'>{result.description}</h6>
-        <p className='text-[#6e767d] text-xs font-medium max-w-[230px]'>Trending with {result?.tags?.map((tag, index) => (
+        <p className='text-[#6e767d] text-xs font-medium max-w-[230px]'>Trending with {result?.tags?.map((tag:string, index:number) => (
           <span key={index} className="tag">{tag}</span>
         ))}</p>
       </div>
